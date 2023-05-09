84442c6c4a71a418dd3f551d45f656a929e91ddb
zhangyongchun <zhangyongchun@planetwalk.com>
2023-05-09 16:17:18 +0800

    init

---
+++ blog-springboot/src/main/java/com/minzheng/blog/BlogApplication.java
@@ -0,0 +1,35 @@
+package com.minzheng.blog;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 博客启动类
+ *
+ * @author yezhiqiu
+ * @date 2021/08/14
+ */
+@MapperScan("com.minzheng.blog.dao")
+@SpringBootApplication
+@EnableScheduling
+@Slf4j
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+        log.info("blog-service 启动成功");
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
